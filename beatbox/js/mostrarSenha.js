f3cadb7918bb7e28579b18f7e733d88f28811f17
antoniomarcossouza <antoniomarcos.souza2002@gmail.com>
2020-10-30 17:18:08 -0300

    v1 de navbar admin e login finalizadas

---
+++ beatbox/js/mostrarSenha.js
@@ -0,0 +1,8 @@
+function mostrarSenha() {
+    var x = document.getElementById("inputPassword");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
