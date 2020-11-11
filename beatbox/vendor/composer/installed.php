53c3ba6bba581201e422710bbb00ca112a26f194
Pedro-Brion <pedro.brion@gmail.com>
2020-11-11 19:49:44 -0300

    Adicionei o composer

---
+++ beatbox/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '91178fb8a43a9286d019b58c8f2ccf23aabb0680',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '91178fb8a43a9286d019b58c8f2ccf23aabb0680',
+    ),
+  ),
+);
