d1794ddfd629078ad67fac5280213cd86455935d
antoniomarcossouza <antoniomarcos.souza2002@gmail.com>
2020-11-14 22:26:25 -0300

    composer instalado | arquivos renomeados | rotas criadas

--- beatbox/app/controllers/Controller.php
+++ beatbox/app/controllers/Controller.php
@@ -1,1 +1,44 @@
-Dentro dessa pasta vai um controller de cada página
+<?php
+
+namespace App\Controllers;
+
+use App\Core\App;
+
+class Controller {
+    public function index(){
+        return require'app/views/site/index.view.php';
+    }
+
+    public function produtos(){
+        return require'app/views/site/View_Produtos.view.php';
+    }
+
+    public function login(){
+        return require'app/views/site/login.view.php';
+    }
+
+    public function quemsomos(){
+        return require'app/views/site/QuemSomos.view.php';
+    }
+
+    public function contato(){
+        return require'app/views/site/Form_contato.view.php';
+    }
+
+    public function admin(){
+        return require'app/views/admin/admin.view.php';
+    }
+
+    public function admin_categorias(){
+        return require'app/views/admin/View_adm_categoria.view.php';
+    }
+
+    public function admin_produtos(){
+        return require'app/views/admin/View_adm_produtos.view.php';
+    }
+
+    public function admin_usuarios(){
+        return require'app/views/admin/View_adm_usuarios.view.php';
+    }
+}
+?>
--- beatbox/app/routes.php
+++ beatbox/app/routes.php
@@ -1,10 +1,19 @@
- <?php
+<?php
  
- //Arquivo de rotas
+ $router->get('','Controller@index');
+
+ $router->get('produtos','Controller@produtos');
+
+ $router->get('contato','Controller@contato');
  
+ $router->get('login','Controller@login');
 
+ $router->get('quemsomos','Controller@quemsomos');
 
+ $router->get('admin','Controller@admin');
 
+ $router->get('admin/categorias','Controller@admin_categorias');
 
+ $router->get('admin/usuarios','Controller@admin_usuarios');
 
-?>
+ $router->get('admin/produtos','Controller@admin_produtos');
