284aaea63b76c9b939e3e5c63e8a528f1a20250e
Pedro-Brion <pedro.brion@gmail.com>
2020-11-21 15:19:13 -0300

    atualizando arquivo do banco de dados

--- beatbox/beatbox.sql
+++ beatbox/beatbox.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.3
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 16, 2020 at 10:35 PM
--- Server version: 10.4.14-MariaDB
--- PHP Version: 7.4.11
+-- Tempo de geração: 21-Nov-2020 às 19:18
+-- Versão do servidor: 10.4.11-MariaDB
+-- versão do PHP: 7.4.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,52 +18,97 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `beatbox`
+-- Banco de dados: `beatbox`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Estrutura da tabela `categorias`
 --
 
-CREATE TABLE `users` (
+CREATE TABLE `categorias` (
   `id` int(11) NOT NULL,
-  `name` varchar(250) NOT NULL,
-  `email` text NOT NULL,
-  `password` varchar(250) NOT NULL
+  `categoria` varchar(191) NOT NULL,
+  `descrição` text NOT NULL,
+  `quantidade` int(80) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `users`
+-- Extraindo dados da tabela `categorias`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `password`) VALUES
-(1, 'Antonio Marcos', 'antoniomarcos@beatbox.com', '123456'),
-(2, 'Pedro', 'pedro@beatbox.com', '45678'),
-(3, 'Larissa', 'larissa@beatbox.com', '12345678'),
-(5, 'Hyago', 'hyago@beatbox.com', '789456'),
-(6, 'Beatriz', 'beatriz@beatbox.com', '4566123');
+INSERT INTO `categorias` (`id`, `categoria`, `descrição`, `quantidade`) VALUES
+(16, 'Violões', 'Instrumento com umas parada de tecitura ai que não entendo', 58),
+(17, 'Guitarras', '', 0),
+(18, 'Baterias', 'Instrumento completo de percussão', 54);
+
+-- --------------------------------------------------------
 
 --
--- Indexes for dumped tables
+-- Estrutura da tabela `produtos`
+--
+
+CREATE TABLE `produtos` (
+  `id` int(11) NOT NULL,
+  `categoria` varchar(191) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `quantidade` int(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `produtos`
+--
+
+INSERT INTO `produtos` (`id`, `categoria`, `nome`, `quantidade`) VALUES
+(1, 'Guitarras', 'Tagima Flying V', 25),
+(2, 'Baterias', 'Batmuito Bateria', 4),
+(3, 'Baterias', 'Batbastante Bateria', 25),
+(4, 'Baterias', 'Batman!!!', 25);
+
+--
+-- Índices para tabelas despejadas
 --
 
 --
--- Indexes for table `users`
+-- Índices para tabela `categorias`
 --
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `categorias`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `categoria` (`categoria`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- Índices para tabela `produtos`
+--
+ALTER TABLE `produtos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `categoria` (`categoria`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
 --
 
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT de tabela `categorias`
 --
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+ALTER TABLE `categorias`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT de tabela `produtos`
+--
+ALTER TABLE `produtos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `produtos`
+--
+ALTER TABLE `produtos`
+  ADD CONSTRAINT `produtos_ibfk_1` FOREIGN KEY (`categoria`) REFERENCES `categorias` (`categoria`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
